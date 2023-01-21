ecb4fac6c5ca8e98a2ed05d27a00c4ad271885e9
Thangible <thangible@gmail.com>
2023-01-22 00:47:28 +0100

    put all the links fix in the classifier, solve the parser problem later

--- parser_config.py
+++ parser_config.py
@@ -6,10 +6,10 @@ def config_parser():
     # training options
     parser.add_argument('--config', is_config_file=True,
                         help='config file path')
-    parser.add_argument("--input_file", type=str,
-                        default='./light_compressed.npz', help='input file')   
+    # parser.add_argument("--input_file", type=str,
+    #                     default='./light_compressed.npz', help='input file')   
     parser.add_argument("--epochs", type=int, default=100,
                         help='number of epochs')  
-    parser.add_argument('--unwanted_classes', type=json.loads)
-    parser.add_argument('--unwanted_pics', type=json.loads)
+    # parser.add_argument('--unwanted_classes', type=json.loads)
+    # parser.add_argument('--unwanted_pics', type=json.loads)
     return parser
