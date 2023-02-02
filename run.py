f4a59504bbe8fff445a03090aeeeee9757820715
Thangible <thangible@gmail.com>
2023-02-02 20:40:39 +0100

    add more augmentation techniques

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import classifier
-import wandb
 from  parser_config import config_parser
 import albumentations as A
 
@@ -11,12 +10,22 @@ if __name__ == "__main__":
     parser = config_parser()
     args  = parser.parse_args()
     
-    center_crop = A.Compose([A.CenterCrop(height = 200, width = 200, p=1.0), 
-                         A.Resize(height = 500, width = 500, interpolation=1, p=1.0)
-                         ])
-    augmentations = [center_crop
-                     ]
-    run_names = ['center crop 200/500']
+    CenterCrop = A.Compose([A.CenterCrop(height = 200, width = 200, p=1.0), 
+                         A.Resize(height = 500, width = 500, interpolation=1, p=1.0)])
+    MotionBlur =  A.MotionBlur(blur_limit =30,  p=1.0)
+    Clahe = A.CLAHE(p=1.0)
+    Sharpen = A.Sharpen(alpha = (0.2, 0.5), lightness = (0.5, 1.0), p=1.0)
+    ColorJitter =  A.ColorJitter( p=1.0)
+    Solarize = A.Solarize(p=1)
+    ToGray = A.ToGray(p=1)
+    
+    augmentations = [MotionBlur,Clahe, Sharpen, ColorJitter]
+    run_names = ['Motion Blur - limit 30',
+                 'Clahe - Default'
+                 'Sharpen - alpha (0.2, 0.5), lightness (0.5,1)',
+                 'ColorJitter - Default',
+                 'Solarize - Default',
+                 'ToGray']
     
     
     for i in range(len(augmentations)):
