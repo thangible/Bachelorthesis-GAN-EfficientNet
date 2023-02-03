4801946fecb8a48063a2de279964afa30efe4e79
Thangible <thangible@gmail.com>
2023-02-04 00:47:57 +0100

    add more augmentation

--- run.py
+++ run.py
@@ -10,25 +10,24 @@ if __name__ == "__main__":
     parser = config_parser()
     args  = parser.parse_args()
     
-    CenterCrop = A.Compose([A.CenterCrop(height = 200, width = 200, p=1.0),
-                            A.Resize(height = 500, width = 500, interpolation=1, p=1.0)])
-    CenterCrop_2 = A.Compose([A.CenterCrop(height = 100, width = 100, p=1.0),
-                            A.Resize(height = 500, width = 500, interpolation=1, p=1.0)])
+    Resize = A.Resize(height = 500, width = 500, interpolation=1, p=1.0)
+    CenterCrop = A.Compose([A.CenterCrop(height = 200, width = 200, p=1.0),Resize])
+    CenterCrop_2 = A.Compose([A.CenterCrop(height = 100, width = 100, p=1.0),Resize])
     MotionBlur =  A.MotionBlur(blur_limit =31,  p=1.0)
     Clahe = A.CLAHE(p=1.0)
-    Sharpen = A.Sharpen(alpha = (0.2, 0.5), lightness = (0.5, 1.0), p=1.0)
+    Sharpen = A.Sharpen(p=1.0)
     ColorJitter =  A.ColorJitter( p=1.0)
     Solarize = A.Solarize(p=1)
     ToGray = A.ToGray(p=1)
-    RandomGridShuffle = A.RandomGridShuffle(grid=(4, 3), p=1.0)
+    RandomGridShuffle = A.RandomGridShuffle(grid=(3, 3), p=1.0)
     Perspective = A.Perspective(scale = (0.2,0.1),  p=1)
+    Posterize = A.Posterize(p= 1)
+    CropThenSharpen = A.Compose([CenterCrop, Sharpen])
     
-    augmentations = [ColorJitter, Solarize, ToGray, CenterCrop_2, RandomGridShuffle, Perspective, Clahe]
-    run_names = ['ColorJitter - Default',
-                 'Solarize - Default',
-                 'ToGray',
-                 'CenterCrop 100/500',
-                 'RandomGridShuffle grid(4,3)',
+    augmentations = [CropThenSharpen, Posterize, Perspective, Clahe]
+    run_names = ['CropthenSharpen',
+                 'Posterize',
+                 'Perspective',
                  'Clahe Default']
     
     
