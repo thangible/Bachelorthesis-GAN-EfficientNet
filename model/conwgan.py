8d51173be38aa8410cd75fb7ed8559414a0e92b4
Thangible <thangible@gmail.com>
2023-02-07 18:25:09 +0100

    desperate

--- model/conwgan.py
+++ model/conwgan.py
@@ -50,7 +50,7 @@ class DepthToSpace(nn.Module):
         output_height = int(input_height * self.block_size)
         t_1 = output.reshape(batch_size, input_height, input_width, self.block_size_sq, output_depth)
         spl = t_1.split(self.block_size, 3)
-        stacks = [t_t.reshape(batch_size,output_height,output_width,output_depth) for t_t in spl]
+        stacks = [t_t.reshape(batch_size,input_height,output_width,output_depth) for t_t in spl]
         output = torch.stack(stacks,0).transpose(0,1).permute(0,2,1,3,4).reshape(batch_size,output_height,output_width,output_depth)
         output = output.permute(0, 3, 1, 2)
         return output
