3996ab86ab01445c2a9ab9c781b79e39795f2bd7
Thangible <thangible@gmail.com>
2023-02-27 01:05:45 +0100

    HP

--- config/parser_config.py
+++ config/parser_config.py
@@ -4,7 +4,7 @@ import json
 def config_parser():
     parser = configargparse.ArgumentParser()
     # training options
-    parser.add_argument('--config', is_config_file=True,help='config file path')
+    parser.add_argument('--config', is_config_file=True, default='input_config', help='config file path')
     # parser.add_argument("--input_file", type=str,
     #                     default='./light_compressed.npz', help='input file')   
     parser.add_argument("--epochs", type=int, default=1,help='number of epochs')  
